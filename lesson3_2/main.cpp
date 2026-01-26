288192cf6221c1f968efcb9619642fbf67fc843d
vcuturic <v.cuturic99@gmail.com>
2026-01-26 17:05:08 +0100

    Base CMake project with SFML support.

---
+++ lesson3_2/main.cpp
@@ -0,0 +1,18 @@
+#include <SFML/Window.hpp>
+
+int main()
+{
+    sf::Window window(sf::VideoMode({ 800, 600 }), "My window");
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        while (const std::optional event = window.pollEvent())
+        {
+            // "close requested" event: we close the window
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+    }
+}
